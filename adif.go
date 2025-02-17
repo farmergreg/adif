e8d08251e4d6c8920e7a6d5fdb39f29ac2f3b3b9
✠ Gregory L. Dietsche ✠ <gregory.dietsche@cuw.edu>
2025-02-16 21:20:37 -0600

    remove old cast

--- adif.go
+++ adif.go
@@ -5,8 +5,8 @@
 package adif
 
 const (
-	TagEOH = string("<EOH>") // TagEOH is the end of header ADIF tag: <EOH>
-	TagEOR = string("<EOR>") // TagEOR is the end of record ADIF tag: <EOR>
+	TagEOH = "<EOH>" // TagEOH is the end of header ADIF tag: <EOH>
+	TagEOR = "<EOR>" // TagEOR is the end of record ADIF tag: <EOR>
 )
 
 const adiHeaderPreamble = "K9CTS AM✠DG ADIF Library\n"
