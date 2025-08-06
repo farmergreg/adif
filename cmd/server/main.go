45918168c2a38013cb57c33faeb2b6231133df9e
Gregory L. Dietsche <gregory.dietsche@cuw.edu>
2025-08-05 21:50:13 -0500

    Multipart File Based Uploads

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bytes"
 	"embed"
 	"encoding/json"
 	"flag"
@@ -11,6 +10,8 @@ import (
 	"io/fs"
 	"log"
 	"net/http"
+	"path"
+	"strings"
 
 	"github.com/hamradiolog-net/adif"
 )
@@ -22,11 +23,10 @@ var staticFiles embed.FS
 var indexHTML string
 
 const (
-	contentType           = "Content-Type"
-	contentTypeJSON       = "application/x-adif-json"
-	contentTypeADI        = "application/x-adif-adi"
-	contentTypeXML        = "application/x-adif-xml"
-	contentTypeAutoDetect = "application/x-adif-auto-detect"
+	contentType     = "Content-Type"
+	contentTypeJSON = "application/x-adif-json"
+	contentTypeADI  = "application/x-adif-adi"
+	contentTypeXML  = "application/x-adif-xml"
 )
 
 var indexTemplate = template.Must(template.New("index").Parse(indexHTML))
@@ -52,73 +52,92 @@ func main() {
 }
 
 func handleIndex(w http.ResponseWriter, r *http.Request) {
-	indexTemplate.Execute(w, struct {
-		ContentTypeADI        string
-		ContentTypeXML        string
-		ContentTypeJSON       string
-		ContentTypeAutoDetect string
-	}{
-		ContentTypeADI:        contentTypeADI,
-		ContentTypeXML:        contentTypeXML,
-		ContentTypeJSON:       contentTypeJSON,
-		ContentTypeAutoDetect: contentTypeAutoDetect,
-	})
+	indexTemplate.Execute(w, nil)
 }
 
 func handleConversion(w http.ResponseWriter, r *http.Request) {
+	if strings.HasPrefix(r.Header.Get(contentType), "multipart/form-data;") {
+		handleMulipartFormData(w, r)
+	} else {
+		handleSimpleApi(w, r)
+	}
+}
+
+func handleSimpleApi(w http.ResponseWriter, r *http.Request) {
 	contentType := r.Header.Get(contentType)
 	data := r.Body
 
-	if contentType == contentTypeAutoDetect {
-		input, err := io.ReadAll(r.Body)
-		if err != nil {
-			http.Error(w, "unable to read http request body", http.StatusBadRequest)
-			return
-		}
-
-		// Naively assume that an ADIF file would not be audacious enough to begin with a comment that contains json
-		input = bytes.TrimLeft(input, " \t\r\n")
-		if input[0] == '{' || input[0] == '[' {
-			contentType = contentTypeJSON
-		} else {
-			contentType = contentTypeADI
-		}
-		data = io.NopCloser(bytes.NewReader(input))
-	}
-
 	switch contentType {
-	case contentTypeJSON: // JSON to ADIF
-		var doc adif.Document
-		if err := json.NewDecoder(data).Decode(&doc); err != nil {
-			http.Error(w, "invalid json input", http.StatusBadRequest)
-			return
-		}
-
-		w.Header().Set(contentType, contentTypeADI)
-		if _, err := doc.WriteTo(w); err != nil {
-			http.Error(w, "unable to write adi output", http.StatusInternalServerError)
-			return
-		}
 	case contentTypeADI: // ADIF to JSON
-		doc := adif.NewDocument()
-		if _, err := doc.ReadFrom(data); err != nil {
-			http.Error(w, "unable to read adi input", http.StatusBadRequest)
-			return
-		}
+		convertAdi(w, data, func(w http.ResponseWriter) {
+			w.Header().Set(contentType, contentTypeJSON)
+		})
 
-		w.Header().Set(contentType, contentTypeJSON)
-
-		encoder := json.NewEncoder(w)
-		encoder.SetIndent("", "  ")
-		if err := encoder.Encode(doc); err != nil {
-			http.Error(w, "unable to create json output", http.StatusInternalServerError)
-			return
-		}
+	case contentTypeJSON: // JSON to ADIF
+		convertAdij(w, data, func(w http.ResponseWriter) {
+			w.Header().Set(contentType, contentTypeADI)
+		})
 
 	default:
 		http.Error(w,
 			fmt.Sprintf("Unsupported %s. Use %s or %s", contentType, contentTypeADI, contentTypeJSON),
 			http.StatusUnsupportedMediaType)
 	}
+}
 
+func handleMulipartFormData(w http.ResponseWriter, r *http.Request) {
+	r.ParseMultipartForm(1024 * 256) // 256KiB
+	file, handler, err := r.FormFile("file")
+	if err != nil {
+		http.Error(w, "Error reading input file", http.StatusBadRequest)
+		return
+	}
+	defer file.Close()
+
+	ext := strings.ToLower(path.Ext(handler.Filename))
+	filename := handler.Filename[:len(handler.Filename)-len(ext)]
+	switch ext {
+	case ".adi":
+		convertAdi(w, file, func(w http.ResponseWriter) {
+			w.Header().Set("Content-Disposition", "attachment; filename="+filename+".adij")
+		})
+
+	case ".adij":
+		convertAdij(w, file, func(w http.ResponseWriter) {
+			w.Header().Set("Content-Disposition", "attachment; filename="+filename+".adi")
+		})
+
+	default:
+		http.Error(w, "files must have .adi or .adij file extension", http.StatusUnsupportedMediaType)
+	}
+}
+
+func convertAdi(w http.ResponseWriter, file io.Reader, beforeWriteCallback func(w http.ResponseWriter)) {
+	doc := adif.NewDocument()
+	if _, err := doc.ReadFrom(file); err != nil {
+		http.Error(w, "unable to read adi input", http.StatusBadRequest)
+		return
+	}
+
+	beforeWriteCallback(w)
+	encoder := json.NewEncoder(w)
+	encoder.SetIndent("", "  ")
+	if err := encoder.Encode(doc); err != nil {
+		http.Error(w, "unable to create json output", http.StatusInternalServerError)
+		return
+	}
+}
+
+func convertAdij(w http.ResponseWriter, file io.Reader, beforeWriteCallback func(w http.ResponseWriter)) {
+	var doc adif.Document
+	if err := json.NewDecoder(file).Decode(&doc); err != nil {
+		http.Error(w, "invalid json input", http.StatusBadRequest)
+		return
+	}
+
+	beforeWriteCallback(w)
+	if _, err := doc.WriteTo(w); err != nil {
+		http.Error(w, "unable to write adi output", http.StatusInternalServerError)
+		return
+	}
 }
