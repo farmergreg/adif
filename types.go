162e81495e4e1016e654a3335028054190e0727e
Gregory L. Dietsche <gregory.dietsche@cuw.edu>
2025-08-31 14:12:57 -0500

    clarify comments

--- types.go
+++ types.go
@@ -9,7 +9,7 @@ import (
 // Future Work:
 // This type intentionally resembles the ADX XML structure even though XML is not currently supported by this library.
 type Document struct {
-	// Header is nil if there is no header.
+	// Header is nil when there is no header.
 	// Otherwise it will be a Record with header fields inside.
 	Header Record `json:"HEADER,omitempty"`
 
@@ -20,6 +20,6 @@ type Document struct {
 	headerPreamble string
 }
 
-// Record is a map of ADIF fields to their values, representing either a header or QSO record.
+// Record is a map of ADIF fields to their values, representing EITHER a Header record or a QSO record.
 // The field keys must be UPPERCASE strings of type adifield.Field.
 type Record map[adifield.ADIField]string
