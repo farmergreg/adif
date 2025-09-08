9e815f35fe10bb463fc2e88c0b2d592b71b6d2ca
Gregory L. Dietsche <gregory.dietsche@cuw.edu>
2025-09-08 10:35:28 -0500

    combine the examples

--- example_adi_test.go
+++ example_adi_test.go
@@ -34,3 +34,30 @@ func ExampleNewADIReader() {
 	// Call: K9CTS, Date: 20230101, Time: 1200, Band: 20M, Mode: SSB
 	// Call: W9PVA, Date: 20230102, Time: 1300, Band: 40M, Mode: CW
 }
+
+// ExampleNewADIWriter demonstrates how to write an ADI document using NewADIWriter.
+func ExampleNewADIWriter() {
+	var sb strings.Builder
+	writer := NewADIWriter(&sb)
+
+	hdr := NewADIRecord()
+	hdr.SetIsHeader(true)
+	hdr.Set(adifield.CREATED_TIMESTAMP, "20250907 212700")
+
+	qso := NewADIRecord()
+	qso.Set(adifield.BAND, "20M")
+	qso.Set(adifield.CALL, "K9CTS")
+	qso.Set(adifield.MODE, "SSB")
+
+	writer.Write([]ADIFRecord{hdr, qso})
+
+	fmt.Println(sb.String())
+
+	// Output:
+	// AM✠DG
+	// K9CTS High Performance ADIF Processing Library
+	// https://github.com/hamradiolog-net/adif-parser
+	//
+	// <CREATED_TIMESTAMP:15>20250907 212700<EOH>
+	// <BAND:3>20M<MODE:3>SSB<CALL:5>K9CTS<EOR>
+}
--- example_adij.go
+++ example_adij.go
@@ -65,3 +65,36 @@ func ExampleNewADIJReader() {
 	// Is Header: false
 	// QSO CALL: W9PVA, BAND: 40M, MODE: CW
 }
+
+// ExampleNewADIJWriter demonstrates how to write ADIJ JSON document using NewADIJWriter.
+func ExampleNewADIJWriter() {
+	var sb strings.Builder
+	writer := NewADIJWriter(&sb)
+
+	hdr := NewADIRecord()
+	hdr.SetIsHeader(true)
+	hdr.Set(adifield.CREATED_TIMESTAMP, "20250907 212700")
+
+	qso := NewADIRecord()
+	qso.Set(adifield.BAND, "20M")
+	qso.Set(adifield.CALL, "K9CTS")
+	qso.Set(adifield.MODE, "SSB")
+
+	writer.Write([]ADIFRecord{hdr, qso})
+
+	fmt.Println(sb.String())
+
+	// Output:
+	// {
+	//   "HEADER": {
+	//     "CREATED_TIMESTAMP": "20250907 212700"
+	//   },
+	//   "RECORDS": [
+	//     {
+	//       "BAND": "20M",
+	//       "CALL": "K9CTS",
+	//       "MODE": "SSB"
+	//     }
+	//   ]
+	// }
+}
