532e77bffe9883e091deb28d7697b2314ed63ce5
Gregory L. Dietsche <gregory.dietsche@cuw.edu>
2025-09-07 21:39:36 -0500

    work on examples

---
+++ example_adijreader_test.go
@@ -0,0 +1,67 @@
+package adif
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/hamradiolog-net/adif-spec/v6/adifield"
+)
+
+// ExampleNewADIJReader demonstrates how to read ADIJ JSON document using NewADIJReader.
+func ExampleNewADIJReader() {
+	jsonExample := `{
+  "HEADER": {
+    "CREATED_TIMESTAMP": "20250907 212700",
+    "PROGRAMID": "ExampleProgram",
+    "PROGRAMVERSION": "1.0"
+  },
+  "RECORDS": [
+    {
+      "BAND": "20M",
+      "CALL": "K9CTS",
+      "MODE": "SSB",
+      "QSO_DATE": "20250907",
+      "TIME_ON": "2127"
+    },
+    {
+      "BAND": "40M",
+      "CALL": "N1ABC",
+      "MODE": "CW",
+      "QSO_DATE": "20250907",
+      "TIME_ON": "2130"
+    }
+  ]
+}`
+
+	// Create a reader from the ADIJ data
+	reader, err := NewADIJReader(strings.NewReader(jsonExample), false)
+	if err != nil {
+		fmt.Printf("Error creating reader: %v\n", err)
+		return
+	}
+
+	// Read all records and collect them
+	for {
+		record, _, err := reader.Next()
+		if err != nil {
+			break // EOF or other error
+		}
+		fmt.Printf("Is Header: %v\n", record.IsHeader())
+		if record.IsHeader() {
+			fmt.Printf("Header CREATED_TIMESTAMP: %s\n", record.Get(adifield.CREATED_TIMESTAMP))
+		} else {
+			fmt.Printf("QSO CALL: %s, BAND: %s, MODE: %s\n", record.Get(adifield.CALL), record.Get(adifield.BAND), record.Get(adifield.MODE))
+		}
+		fmt.Println()
+	}
+
+	// Output:
+	// Is Header: true
+	// Header CREATED_TIMESTAMP: 20250907 212700
+	//
+	// Is Header: false
+	// QSO CALL: K9CTS, BAND: 20M, MODE: SSB
+	//
+	// Is Header: false
+	// QSO CALL: N1ABC, BAND: 40M, MODE: CW
+}
--- example_adijwriter_test.go
+++ example_adijwriter_test.go
@@ -7,23 +7,29 @@ import (
 	"github.com/hamradiolog-net/adif-spec/v6/adifield"
 )
 
+// ExampleNewADIJWriter demonstrates how to write ADIJ JSON document using NewADIJWriter.
 func ExampleNewADIJWriter() {
-
 	var sb strings.Builder
 	writer := NewADIJWriter(&sb)
 
+	hdr := NewADIRecord()
+	hdr.SetIsHeader(true)
+	hdr.Set(adifield.CREATED_TIMESTAMP, "20250907 212700")
+
 	qso := NewADIRecord()
-	qso.SetIsHeader(false)
 	qso.Set(adifield.BAND, "20M")
 	qso.Set(adifield.CALL, "K9CTS")
 	qso.Set(adifield.MODE, "SSB")
 
-	writer.Write([]ADIFRecord{qso})
+	writer.Write([]ADIFRecord{hdr, qso})
 
 	fmt.Println(sb.String())
 
 	// Output:
 	// {
+	//   "HEADER": {
+	//     "CREATED_TIMESTAMP": "20250907 212700"
+	//   },
 	//   "RECORDS": [
 	//     {
 	//       "BAND": "20M",
