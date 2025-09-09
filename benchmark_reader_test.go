c84439784e145fbb4c55df7b11edfd9cd78a016e
Gregory L. Dietsche <gregory.dietsche@cuw.edu>
2025-09-08 20:01:37 -0500

    organize benchmarks

---
+++ benchmark_reader_test.go
@@ -0,0 +1,70 @@
+package adif
+
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"strings"
+	"testing"
+)
+
+func BenchmarkADIRead(b *testing.B) {
+	var qsoList []ADIFRecord
+	b.ResetTimer()
+	for b.Loop() {
+		qsoList = make([]ADIFRecord, 0, 10000)
+		p := NewADIReader(strings.NewReader(benchmarkFile), false)
+		for {
+			q, err := p.Next()
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				b.Fatal(err)
+			}
+			qsoList = append(qsoList, q)
+		}
+	}
+
+	_ = len(qsoList)
+}
+
+func BenchmarkADIJRead(b *testing.B) {
+	var qsoList []ADIFRecord
+	json := benchmarkFileAsJSON()
+	b.ResetTimer()
+	for b.Loop() {
+		qsoList = make([]ADIFRecord, 0, 10000)
+		p, err := NewADIJReader(bytes.NewReader(json), false)
+		if err != nil {
+			b.Fatal(err)
+		}
+		if err != nil {
+			b.Fatal(err)
+		}
+		for {
+			q, err := p.Next()
+			if err == io.EOF {
+				break
+			}
+			qsoList = append(qsoList, q)
+		}
+	}
+
+	_ = len(qsoList)
+}
+
+// This benchmark works directly on JSON data, without using this library for reading the JSON ADIF data.
+// It is meant as a reference point to compare the performance to a known standard (the go stdlib JSON parser).
+func BenchmarkJSONReadReference(b *testing.B) {
+	jsonRecords := benchmarkFileAsJSON()
+
+	b.ResetTimer()
+	document := adifDocument{}
+	for b.Loop() {
+		err := json.Unmarshal(jsonRecords, &document)
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+}
