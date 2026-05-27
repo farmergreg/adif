2eae4f1a73ed756a4ccb0f20dc06e3e2d3fed25f
Gregory L. Dietsche <gregory.dietsche@cuw.edu>
2026-05-26 21:42:52 -0500

    remove unused const

--- errors.go
+++ errors.go
@@ -6,9 +6,6 @@ var (
 	// ErrAdiReaderMalformedADI is returned when the ADI formatted data does not conform to the ADIF specification.
 	ErrAdiReaderMalformedADI = errors.New("adi reader: data is malformed")
 
-	// ErrAdiReaderNilReader is returned when the reader passed to NewADIDocumentReader is nil.
-	ErrAdiWriterNilWriter = errors.New("adi writer: nil writer")
-
 	// ErrHeaderAlreadyWritten is returned when attempting to write more than one header record.
 	ErrHeaderAlreadyWritten = errors.New("header record already written")
 )
